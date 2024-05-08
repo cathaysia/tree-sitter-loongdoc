3e4ddcff9d5457f34889faa7fdc5d4b56170c9d4
DragonBillow <DragonBillow@outlook.com>
2024-05-08 17:41:05 +0800

    feat: add list
    Signed-off-by: DragonBillow <DragonBillow@outlook.com>

--- tree-sitter-asciidoc/grammar.js
+++ tree-sitter-asciidoc/grammar.js
@@ -10,19 +10,69 @@ module.exports = grammar({
     $.title_h3_marker,
     $.title_h4_marker,
     $.title_h5_marker,
+    $.list_marker_star,
+    $.list_marker_hyphen,
+    $.list_marker_dot,
+    $.list_marker_digit,
+    $.list_marker_geek,
+    $.list_marker_alpha,
   ],
 
+  precedences: $ => [[$.checked_list, $.unordered_list]],
+
   rules: {
-    document: $ => repeat($._title_block),
-    _title_block: $ =>
-      choice($.title0, $.title1, $.title2, $.title3, $.title4, $.title5),
-    title0: $ => seq($.title_h0_marker, /[^\n]+/, $._block_end),
-    title1: $ => seq($.title_h1_marker, /[^\n]+/, $._block_end),
-    title2: $ => seq($.title_h2_marker, /[^\n]+/, $._block_end),
-    title3: $ => seq($.title_h3_marker, /[^\n]+/, $._block_end),
-    title4: $ => seq($.title_h4_marker, /[^\n]+/, $._block_end),
-    title5: $ => seq($.title_h5_marker, /[^\n]+/, $._block_end),
+    document: $ =>
+      repeat(
+        choice(
+          $.title0,
+          $.title1,
+          $.title2,
+          $.title3,
+          $.title4,
+          $.title5,
+          $.unordered_list,
+          $.ordered_list,
+          $.checked_list,
+        ),
+      ),
+    title0: $ => seq($.title_h0_marker, $._WHITE_SPACE, $.line),
+    title1: $ => seq($.title_h1_marker, $._WHITE_SPACE, $.line),
+    title2: $ => seq($.title_h2_marker, $._WHITE_SPACE, $.line),
+    title3: $ => seq($.title_h3_marker, $._WHITE_SPACE, $.line),
+    title4: $ => seq($.title_h4_marker, $._WHITE_SPACE, $.line),
+    title5: $ => seq($.title_h5_marker, $._WHITE_SPACE, $.line),
+
+    checked_list: $ => seq(repeat1($.ul_item), $._block_end),
+    ck_item: $ =>
+      seq(
+        $.unordered_list_marker,
+        $._WHITE_SPACE,
+        '[',
+        choice(' ', 'x', '*'),
+        ']',
+        $._WHITE_SPACE,
+        $.line,
+      ),
+
+    unordered_list: $ => seq(repeat1($.ul_item), $._block_end),
+    ul_item: $ => seq($.unordered_list_marker, $._WHITE_SPACE, $.line),
+    unordered_list_marker: $ =>
+      choice($.list_marker_star, $.list_marker_hyphen),
+
+    ordered_list: $ => seq(repeat1($.ol_item), $._block_end),
+    ol_item: $ => seq($.ordered_list_marker, $._WHITE_SPACE, $.line),
+    ordered_list_marker: $ =>
+      choice(
+        $.list_marker_digit,
+        $.list_marker_geek,
+        $.list_marker_alpha,
+        $.list_marker_dot,
+      ),
+
+    line: $ => seq(/[^\n]+/, $._block_end),
+
     _block_end: $ => choice($._NEWLINE, $._eof),
     _NEWLINE: _ => /\r?\n/,
+    _WHITE_SPACE: $ => /[ \t]+/,
   },
 })
--- tree-sitter-asciidoc/src/scanner.c
+++ tree-sitter-asciidoc/src/scanner.c
@@ -8,7 +8,20 @@ typedef enum TokenType {
     TOKEN_TITLE_H3_MARKER,
     TOKEN_TITLE_H4_MARKER,
     TOKEN_TITLE_H5_MARKER,
+    TOKEN_LIST_MARKER_STAR,
+    TOKEN_LIST_MARKER_HYPHEN,
+    TOKEN_LIST_MARKER_DOT,
+    TOKEN_LIST_MARKER_DIGIT,
+    TOKEN_LIST_MARKER_GEEK,
+    TOKEN_LIST_MARKER_ALPHA,
 } TokenType;
+
+static bool parse_unordered_marker(char start, TSLexer *lexer, const bool *valid_symbols);
+static bool parse_ordered_marker(TSLexer *lexer, const bool *valid_symbols);
+static bool is_white_space(int32_t ch);
+static bool is_ascii_digit(int32_t ch);
+static bool is_ascii_alpha_lower(int32_t ch);
+static bool is_geek_lower(int32_t ch);
 
 void *tree_sitter_asciidoc_external_scanner_create() {
     return NULL;
@@ -35,29 +48,114 @@ bool tree_sitter_asciidoc_external_scanner_scan(void *payload, TSLexer *lexer, c
     }
 
     if(lexer->get_column(lexer) == 0) {
+        if(parse_ordered_marker(lexer, valid_symbols)) {
+            return true;
+        }
         switch(lexer->lookahead) {
             case '=': {
-                lexer->advance(lexer, false);
                 if(!valid_symbols[TOKEN_TITLE_H0_MARKER]) {
                     break;
                 }
-                unsigned level = TOKEN_TITLE_H0_MARKER;
+                unsigned level = TOKEN_TITLE_H0_MARKER - 1;
                 while(lexer->lookahead == '=') {
                     lexer->advance(lexer, false);
+                    lexer->mark_end(lexer);
                     ++level;
                 }
                 if(level > TOKEN_TITLE_H5_MARKER) {
                     return false;
                 }
-                if(lexer->lookahead != ' ') {
+                if(!is_white_space(lexer->lookahead)) {
                     return false;
                 }
-                lexer->mark_end(lexer);
                 lexer->result_symbol = level;
                 return true;
+            }
+            case '*': {
+                lexer->result_symbol = TOKEN_LIST_MARKER_STAR;
+                if(parse_unordered_marker('*', lexer, valid_symbols)) {
+                    return true;
+                }
+            }
+            case '-': {
+                lexer->result_symbol = TOKEN_LIST_MARKER_HYPHEN;
+                if(parse_unordered_marker('-', lexer, valid_symbols)) {
+                    return true;
+                }
+            }
+            case '.': {
+                lexer->result_symbol = TOKEN_LIST_MARKER_DOT;
+                if(parse_unordered_marker('.', lexer, valid_symbols)) {
+                    return true;
+                }
             }
         }
     }
 
     return false;
+}
+
+bool parse_unordered_marker(char start, TSLexer *lexer, const bool *valid_symbols) {
+    if(lexer->get_column(lexer) != 0) {
+        return false;
+    }
+    while(lexer->lookahead == start) {
+        lexer->advance(lexer, false);
+    }
+    if(!is_white_space(lexer->lookahead)) {
+        return false;
+    }
+    lexer->mark_end(lexer);
+
+    return true;
+}
+
+bool parse_ordered_marker(TSLexer *lexer, const bool *valid_symbols) {
+    if(lexer->get_column(lexer) != 0) {
+        return false;
+    }
+
+    typedef bool (*FnJudge)(int32_t);
+    FnJudge is_order_marker;
+    if(is_ascii_digit(lexer->lookahead)) {
+        is_order_marker = is_ascii_digit;
+        lexer->result_symbol = TOKEN_LIST_MARKER_DIGIT;
+    } else if(is_ascii_alpha_lower(lexer->lookahead)) {
+        is_order_marker = is_ascii_alpha_lower;
+        lexer->result_symbol = TOKEN_LIST_MARKER_ALPHA;
+    } else if(is_geek_lower(lexer->lookahead)) {
+        is_order_marker = is_geek_lower;
+        lexer->result_symbol = TOKEN_LIST_MARKER_GEEK;
+    } else {
+        return false;
+    }
+
+    while(is_order_marker(lexer->lookahead)) {
+        lexer->advance(lexer, false);
+    }
+    if(lexer->lookahead != '.') {
+        return false;
+    }
+    lexer->advance(lexer, false);
+    if(!is_white_space(lexer->lookahead)) {
+        return false;
+    }
+    lexer->mark_end(lexer);
+    return true;
+}
+
+static bool is_white_space(int32_t ch) {
+    return ch == ' ' || ch == '\t';
+}
+
+static bool is_ascii_digit(int32_t ch) {
+    return ch >= '0' && ch <= '9';
+}
+
+static bool is_ascii_alpha_lower(int32_t ch) {
+    return ch >= 'a' && ch <= 'z';
+}
+
+static bool is_geek_lower(int32_t ch) {
+    return ch >= 945 && ch <= 969;
 }
