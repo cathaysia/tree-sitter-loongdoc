f50b211cae7342355045efb735048456dfbd542d
DragonBillow <DragonBillow@outlook.com>
2024-05-23 00:50:11 +0800

    feat(adoc): update highlight
    Signed-off-by: DragonBillow <DragonBillow@outlook.com>

--- tree-sitter-asciidoc/queries/highlights.scm
+++ tree-sitter-asciidoc/queries/highlights.scm
@@ -1,6 +1,26 @@
-(title_h0_marker) @markup.heading.1
-(title_h1_marker) @markup.heading.2
-(title_h2_marker) @markup.heading.3
-(title_h3_marker) @markup.heading.4
-(title_h4_marker) @markup.heading.5
-(title_h5_marker) @markup.heading.6
+(title0) @markup.heading.1
+(title1) @markup.heading.2
+(title2) @markup.heading.3
+(title3) @markup.heading.4
+(title4) @markup.heading.5
+(title5) @markup.heading.6
+
+(table_block_marker) @punctuation.special
+(table_cell_attr) @attribute
+(table_cell
+  "|" @punctuation.special)
+
+(breaks) @punctuation.special
+
+(task_list_marker_unchecked) @markup.list.unchecked
+
+(task_list_marker_checked) @markup.list.checked
+
+[
+  (list_marker_star)
+  (list_marker_hyphen)
+  (list_marker_dot)
+  (list_marker_digit)
+  (list_marker_geek)
+  (list_marker_alpha)
+] @markup.list
