3376ed24eb6d43156f549b74ec303b7411795922
DragonBillow <DragonBillow@outlook.com>
2024-05-27 14:13:52 +0800

    feat(adoc): allow raw block appears inside delimited block
    Signed-off-by: DragonBillow <DragonBillow@outlook.com>

--- tree-sitter-asciidoc/grammar.js
+++ tree-sitter-asciidoc/grammar.js
@@ -213,7 +213,19 @@ module.exports = grammar({
       ),
 
     delimited_block: $ =>
-      seq($.delimited_block_marker, repeat($.line), $.delimited_block_marker),
+      seq(
+        $.delimited_block_marker,
+        repeat(
+          choice(
+            $.line,
+            alias(
+              seq(repeat(choice($.element_attr, $.block_title)), $.raw_block),
+              $.section_block,
+            ),
+          ),
+        ),
+        $.delimited_block_marker,
+      ),
     open_block: $ =>
       seq($.open_block_marker, repeat($.line), $.open_block_marker),
     raw_block: $ =>
