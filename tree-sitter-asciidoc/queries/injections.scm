f260e775c11d9c7235dedfb2338b6161794b1465
DragonBillow <DragonBillow@outlook.com>
2024-05-30 10:47:09 +0800

    feat(adoc): add code_block injection
    Signed-off-by: DragonBillow <DragonBillow@outlook.com>

--- tree-sitter-asciidoc/queries/injections.scm
+++ tree-sitter-asciidoc/queries/injections.scm
@@ -5,3 +5,27 @@
 ((line) @injection.content
   (#set! injection.include-children)
   (#set! injection.language "asciidoc_inline"))
+
+((section_block
+  (element_attr
+    (element_attr_marker)
+    (attr_value) @injection.language
+    (element_attr_marker))
+  (raw_block
+    (raw_block_marker)
+    (raw_block_body) @injection.content
+    (raw_block_marker)))
+  (#gsub! @injection.language "%s*,%s*(%w+)%s*" "%1")
+  (#vim-match? @injection.language "\\s*,\\s*.*\\s*"))
+
+((section_block
+  (element_attr
+    (element_attr_marker)
+    (attr_value) @injection.language
+    (element_attr_marker))
+  (raw_block
+    (raw_block_marker)
+    (raw_block_body) @injection.content
+    (raw_block_marker)))
+  (#gsub! @injection.language "%s*source%s*,%s*(%w+)%s*" "%1")
+  (#vim-match? @injection.language "\\s*source\\s*,\\s*.*\\s*"))
