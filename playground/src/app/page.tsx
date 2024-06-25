b9828624ee19d224cbf257ab1755ca8052de42cc
DragonBillow <DragonBillow@outlook.com>
2024-06-25 22:47:50 +0800

    feat(playground): allow load from file
    Signed-off-by: DragonBillow <DragonBillow@outlook.com>

--- playground/src/app/page.tsx
+++ playground/src/app/page.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 import { LoongDoc } from './loong_doc'
 import Editor from './editor'
 import { useState } from 'react'
+import { LoadFile } from './load_file'
 
 const EXAMPLE = `
 = title0
@@ -22,13 +23,20 @@ export default function Home() {
 
   return (
     <main className="h-screen w-full flex">
-      <Editor
-        className="h-full w-1/2"
-        value={value}
-        onValueChanged={value => {
-          setValue(value)
-        }}
-      />
+      <div className="h-full w-1/2">
+        <LoadFile
+          onValueChanged={value => {
+            setValue(value)
+          }}
+        />
+        <Editor
+          className="h-full"
+          value={value}
+          onValueChanged={value => {
+            setValue(value)
+          }}
+        />
+      </div>
       <LoongDoc className="h-full w-1/2 overflow-auto bg-white" text={value} />
     </main>
   )
