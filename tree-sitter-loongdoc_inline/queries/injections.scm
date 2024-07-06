11c78c357fe8161927e6746b736b192995247213
DragonBillow <DragonBillow@outlook.com>
2024-07-06 08:31:02 +0800

    feat(queries): inline add asciimath
    Signed-off-by: DragonBillow <DragonBillow@outlook.com>

--- tree-sitter-loongdoc_inline/queries/injections.scm
+++ tree-sitter-loongdoc_inline/queries/injections.scm
@@ -1,3 +1,12 @@
 ((stem_macro
+  [
+    "stem"
+    "latexmath"
+  ]
   (attr) @injection.content)
   (#set! injection.language "latex"))
+
+((stem_macro
+  "asciimath"
+  (attr) @injection.content)
+  (#set! injection.language "asciimath"))
